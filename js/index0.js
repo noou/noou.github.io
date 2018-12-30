7edca2a09a163bcd88e73053e5add188da72772e
noou <cloxca@gmail.com>
2018-12-30 18:28:22 +0500

    fix 490px, add map img

--- js/index0.js
+++
@@ -1,7 +0,0 @@
-$(document).on("scroll", function () {
-	if ($(document).scrollTop() > 100) {
-		$("#navigation-menu").addClass("small");
-	} else {
-		$("#navigation-menu").removeClass("small");
-	}
-});
