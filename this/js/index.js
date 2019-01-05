93409775917e081aa5b5c1817e84166c784851bc
noou <cloxca@gmail.com>
2019-01-05 18:38:37 +0500

    fix svganimation, add p.site

---
+++ this/js/index.js
@@ -0,0 +1,22 @@
+// #Pinspiration
+// https://www.pinterest.com/pin/70228075417474245/
+// https://www.behance.net/gallery/19198309/Ce-Soir-Restaurant
+
+// Fun dummy text
+// http://hipsum.co/
+
+$( document ).ready(function() {
+    // Nav-bar Scroll Function
+    $(".nav-link").click(function(){
+        if($(this.hash).height() > $(window).height()) {
+            $('html,body').animate({
+                scrollTop: $(this.hash).offset().top-25
+            }, 1000);
+        } else {
+            $('html,body').animate({
+                scrollTop: $(this.hash).offset().top-(($(window).height() - $(this.hash).innerHeight())/2)
+            }, 1000);
+        }
+        return false;
+    });
+});
