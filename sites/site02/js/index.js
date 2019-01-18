e4e2f728f439431c5f11abb518b2ee37d0ce5ae1
noou <cloxca@gmail.com>
2019-01-18 17:59:34 +0500

    remake site02 and add site 09

--- sites/site02/js/index.js
+++ sites/site02/js/index.js
@@ -1,44 +1,44 @@
 class StickyNavigation {
-	
+
 	constructor() {
 		this.currentId = null;
 		this.currentTab = null;
 		this.tabContainerHeight = 70;
 		let self = this;
-		$('.et-hero-tab').click(function() { 
-			self.onTabClick(event, $(this)); 
+		$('.et-hero-tab').click(function() {
+			self.onTabClick(event, $(this));
 		});
 		$(window).scroll(() => { this.onScroll(); });
 		$(window).resize(() => { this.onResize(); });
 	}
-	
+
 	onTabClick(event, element) {
 		event.preventDefault();
 		let scrollTop = $(element.attr('href')).offset().top - this.tabContainerHeight + 1;
 		$('html, body').animate({ scrollTop: scrollTop }, 600);
 	}
-	
+
 	onScroll() {
 		this.checkTabContainerPosition();
     this.findCurrentTabSelector();
 	}
-	
+
 	onResize() {
 		if(this.currentId) {
 			this.setSliderCss();
 		}
 	}
-	
+
 	checkTabContainerPosition() {
 		let offset = $('.et-hero-tabs').offset().top + $('.et-hero-tabs').height() - this.tabContainerHeight;
 		if($(window).scrollTop() > offset) {
 			$('.et-hero-tabs-container').addClass('et-hero-tabs-container--top');
-		} 
+		}
 		else {
 			$('.et-hero-tabs-container').removeClass('et-hero-tabs-container--top');
 		}
 	}
-	
+
 	findCurrentTabSelector(element) {
 		let newCurrentId;
 		let newCurrentTab;
@@ -58,7 +58,7 @@ class StickyNavigation {
 			this.setSliderCss();
 		}
 	}
-	
+
 	setSliderCss() {
 		let width = 0;
 		let left = 0;
@@ -69,7 +69,47 @@ class StickyNavigation {
 		$('.et-hero-tab-slider').css('width', width);
 		$('.et-hero-tab-slider').css('left', left);
 	}
-	
+
 }
 
 new StickyNavigation();
+
+
+$(window).scroll(function() {
+  const a = $(this).scrollTop(),
+    b = 800;
+  $("h1").css({ backgroundPosition: "center " + a / 2 + "px" });
+  $(".parallax").css({ top: a / 1.6 + "px", opacity: 1 - a / b });
+});
+
+
+
+// parallax scrolling
+
+document.addEventListener("scroll", () => {
+  const
+    top = window.pageYOffset,
+	topp = 50+window.pageYOffset,
+    one = document.querySelector(".one"),
+    two = document.querySelector(".two"),
+    three = document.querySelector(".three"),
+    four = document.querySelector(".four"),
+    five = document.querySelector(".five"),
+    six = document.querySelector(".six"),
+    seven = document.querySelector(".seven");
+		// paral = document.querySelector(".header .parallax div");
+  // $("header .parallax div").css('background-size', '+=1%');
+  one.style.bottom = -(top * 0.1) + "px";
+  two.style.bottom = -(top * 0.2) + "px";
+  three.style.bottom = -(top * 0.3) + "px";
+  four.style.bottom = -(top * 0.4) + "px";
+  five.style.bottom = -(top * 0.5) + "px";
+  six.style.bottom = -(top * 0.4) + "px";
+  seven.style.bottom = -(top * 0.3) + "px";
+		// paral.style['background-size'] = -(    background-size * 0.1) + "%";
+});
+
+$("header .parallax div").css({ backgroundSize: 100 + (300 - $(window).width() / 10) + "%" });
+$(window).resize(function() {
+    $("header .parallax div").css({ backgroundSize: 100 + (300 - $(window).width() / 10) + "%" });
+});
