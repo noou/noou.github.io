b48adbaaf1fe2ba776246419742cb1c99b826560
noou <cloxca@gmail.com>
2019-01-25 19:08:35 +0500

    site 10

--- sites/site10/js/particle.js
+++ sites/site10/js/particle.js
@@ -106,7 +106,7 @@ particlesJS("particles-js", {
 var count_particles, stats, update;
 stats = new Stats;
 stats.setMode(0);
-stats.domElement.style.position = 'absolute';
+stats.domElement.style.position = 'fixed';
 stats.domElement.style.left = '0px';
 stats.domElement.style.top = '0px';
 document.body.appendChild(stats.domElement);
