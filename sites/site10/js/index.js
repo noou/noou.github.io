21489fba62e998d137dcb52108ce5b5155c0c44f
noou <cloxca@gmail.com>
2019-01-30 19:11:19 +0500

    site 10

--- sites/site10/js/index.js
+++ sites/site10/js/index.js
@@ -1,78 +1,35 @@
+// #### ICONS
 feather.replace()
-// Sticky Header
-$(window).scroll(function() {
+// ###################################################################################
+// #### SLIDES
+var slideIndex = 1;
+showSlides(slideIndex);
 
-    if ($(window).scrollTop() > 100) {
-        $('.main_header').addClass('sticky');
-    } else {
-        $('.main_header').removeClass('sticky');
-    }
-});
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
 
-// Mobile Navigation
-$('.mobile-toggle').click(function() {
-    if ($('.main_header').hasClass('open-nav')) {
-        $('.main_header').removeClass('open-nav');
-    } else {
-        $('.main_header').addClass('open-nav');
-    }
-});
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
 
-$('.main_header li a').click(function() {
-    if ($('.main_header').hasClass('open-nav')) {
-        $('.navigation').removeClass('open-nav');
-        $('.main_header').removeClass('open-nav');
-    }
-});
-
-// navigation scroll
-$('nav a').click(function(event) {
-    var id = $(this).attr("href");
-    var offset = 0;
-    var target = $(id).offset().top - offset;
-    $('html, body').animate({
-        scrollTop: target
-    }, 500);
-    event.preventDefault();
-});
-/* Scroll-to-Top Button */
-$(window).scroll(function () {
-    if ($(this).scrollTop() > 100) {
-        $('.scrollup').fadeIn();
-    } else {
-        $('.scrollup').fadeOut();
-    }
-});
-
-$('.scrollup').click(function () {
-    $("html, body").animate({
-        scrollTop: 0
-    }, 600);
-    return false;
-});
-/* WORK IN PROGRESS
-   NAVIGATION ACTIVE STATE IN SECTION AREA
-*/
-// var sections = $('section'), nav = $('nav'), nav_height = nav.outerHeight();
-//
-// $(window).on('scroll', function () {
-//   var cur_pos = $(this).scrollTop();
-//
-//   sections.each(function() {
-//     var top = $(this).offset().top - nav_height,
-//         bottom = top + $(this).outerHeight();
-//
-//     if (cur_pos >= top && cur_pos <= bottom) {
-//       nav.find('a').removeClass('active');
-//       sections.removeClass('active');
-//
-//       $(this).addClass('active');
-//       nav.find('a[href="#'+$(this).attr('id')+'"]').addClass('active');
-//     }
-//   });
-// });
-
-
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
+// ###################################################################################
+// #### ACCORDION
 var acc = document.getElementsByClassName("accordion");
 var i;
 
@@ -87,7 +44,8 @@ for (i = 0; i < acc.length; i++) {
     }
   });
 }
-
+// ###################################################################################
+// #### TABS
 function openCity(evt, cityName) {
   var i, tabcontent, tablinks;
   tabcontent = document.getElementsByClassName("tabcontent");
@@ -101,12 +59,10 @@ function openCity(evt, cityName) {
   document.getElementById(cityName).style.display = "block";
   evt.currentTarget.className += " active";
 }
-
 // Get the element with id="defaultOpen" and click on it
 document.getElementById("defaultOpen").click();
-
-
-
+// ###################################################################################
+// #### SCROLL INDICATOR
 // When the user scrolls the page, execute myFunction
 window.onscroll = function() {myFunction()};
 
@@ -116,3 +72,83 @@ function myFunction() {
   var scrolled = (winScroll / height) * 100;
   document.getElementById("myBar").style.width = scrolled + "%";
 }
+// ###################################################################################
+// #### NAVI SCROLL
+// navigation scroll
+$('nav a').click(function(event) {
+    var id = $(this).attr("href");
+    var offset = 50;
+    var target = $(id).offset().top - offset;
+    $('html, body').animate({
+        scrollTop: target
+    }, 500);
+    event.preventDefault();
+});
+
+
+$('.chevron').click(function(event) {
+    var id = $(this).attr("href");
+    var offset = 50;
+    var target = $(id).offset().top - offset;
+    $('html, body').animate({
+        scrollTop: target
+    }, 500);
+    event.preventDefault();
+});
+// ###################################################################################
+// #### SCROLL UP
+$(window).scroll(function () {
+    if ($(this).scrollTop() > 100) {
+        $('.scrollup').fadeIn(500);
+    } else {
+        $('.scrollup').fadeOut(500);
+    }
+});
+
+$('.scrollup').click(function () {
+    $("html, body").animate({
+        scrollTop: 0
+    }, 600);
+    return false;
+});
+
+$(window).scroll(function () {
+    if ($(this).scrollTop() < 400) {
+        $('.chevron').fadeIn(500);
+    } else {
+        $('.chevron').fadeOut(500);
+    }
+});
+// ###################################################################################
+
+// Sticky Header
+$(window).scroll(function() {
+
+    if ($(window).scrollTop() > 100) {
+        $('.main_header').addClass('visibil');
+    } else {
+        $('.main_header').removeClass('visibil');
+    }
+});
+
+// Mobile Navigation
+$('.mobile-toggle').click(function() {
+    if ($('.main_nav').hasClass('open-nav')) {
+        $('.main_nav').removeClass('open-nav');
+        $('.navimage').removeClass('open-logo');
+        $('.mobile-toggle').removeClass('toggle-anim');
+    } else {
+        $('.main_nav').addClass('open-nav');
+        $('.navimage').addClass('open-logo');
+        $('.mobile-toggle').addClass('toggle-anim');
+    }
+});
+
+$('.main_header li a').click(function() {
+    if ($('.main_nav').hasClass('open-nav')) {
+        $('.navigation').removeClass('open-nav');
+        $('.main_nav').removeClass('open-nav');
+        $('.navimage').removeClass('open-logo');
+        $('.mobile-toggle').removeClass('toggle-anim');
+    }
+});
